eaf1b3493fc102147e72afce90adf5386ba26953
MrDerpus <47982738+MrDerpus@users.noreply.github.com>
2025-11-24 05:12:30 +1100

    v1.1.0
    Added multi-table support in files.
    Added strict typing, which allows you to returns rows as either (Strings, Integers, Floats or Booleans).
    Added auto detection for new tables.
    Added Clean python dictionary output.

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 Author: MrDerpus
 Python version: 3.12.3
 
-TOME Parser v1.0.0
+TOME Parser v1.1.0
 Table Oriented Markup Encoding.
 
 I just wanted a good looking data structure that is easy read.
@@ -12,49 +12,80 @@ Also because I saw MEMEs about Ai bros mentioning: TOON.
 So I was inspired to create my own parser for my custom data structure.
 
 This combines the simplicity of:
-writing in a SQL-like data table structure,
-CSV data entry,
-and getting the output in a python JSON parser-like format.
-
-It's also made in a day, I expect you to break it very easily.
+- writing in a SQL-like database structure,
+- CSV rows for data entry,
+- and receiving clean python friendly dictionaries.
 ```
 
+# 🪄 A super big update! v1.1.0
+What was added? <br>
+* Multi-table support.
+* Strict typing for table headers (Optional).
+* Refactored syntax dictionary.
+* Auto-detect new tables (Only if formatting is compatible).
+* Clean Python dictionary output structure.
 
-## Rules:
+### Make sure to read the rules to see how to use these new changes.
+
+
+
+## 📜 Rules:
 * Header defines the table name and ordered column list.
 * Data rows map directly to the column list.
 * Indentation is optional.
-* Commas "," define the field.
-* Leading and trailing whitespace is ignored.
+* Commas "," separate fields.
+* Leading and trailing whitespace is purged for your convenience.
 * Lines beginning "#" or ";" are treated as comments.
-* A line beginning with "--" will end the parsing early.
-* Currently returns all values as strings.
+* A line beginning with "!"  will end the parsing early.
+* A line beginning with "==" will tell the parser to start paring a new data table.
+* A blank line separating two tables will also automatically trigger parsing for another data table.
+* By default, all values are returned as strings, unless specified otherwise.
 
-## Proposed changes:
-* Make values return their proper type.
 
-## Returns a dictionary in this format:
+## TOME returns a dictionary in this format:
 ```python
 {
-	"table_name": <name>,
-	0:{col1: value, col2: value, ...},
-	1{...},
-	...
+    'items': [
+        {
+            'serial': '2ffc-4eb3-8a2f',
+            'item': 'keyboard',
+            'qty': '50',
+            'price': '36.99',
+            'sale': 'true'
+        },
+        {
+            'serial': 'a6eb-4c71-b501',
+			...
+		}
+	]
 }
 ```
 
-## And can be accessed in python like this:
+## Accessing values in Python:
 ```python
-output = TOME.read(./path/to/file/test.t)
-table_name = output["TableName"]
-first_item = output[0][...]
+output = TOME.read('./path/to/file/test.t')
+first_item_serial = output['items'][0]['serial']
 ```
+---
 
+## Demos:
 ```ini
-; TOME example:
-employees[uuid, name]
-	c0fb3d3b-2ffc-4eb3-8a2f-8177aeebecd3, John Doe
-	ab5573e2-a6eb-4c71-b501-7a9206e43a1f, Jane Doe
-	d7efb46c-a512-4eda-b55f-64d7f888faf3, Bill Doe
---
+# Default parsing, will return all values as strings:
+employees[uuid, name, age]
+	c0fb3d3b-2ffc-4eb3-8a2f-8177aeebecd3, John Doe, 35
+	ab5573e2-a6eb-4c71-b501-7a9206e43a1f, Jane Doe, 21
+	d7efb46c-a512-4eda-b55f-64d7f888faf3, Bill Doe, 42
+
+# Strict parsing, only selected columns are type-cast.
+# Columns without a type remain as strings.
+items[serial:str, item, qty:int, price:float, sale:bool]:
+	2ffc-4eb3-8a2f, keyboard,  50,  36.99,   true
+	a6eb-4c71-b501, monitor,   27,  139.98,  true
+	a512-4eda-b55f, mouse,     23,  20.99,   false
+
+; Supported data types:
+; String/str
+; Integer/int
+; Boolean/bool
+; Float
 ```
--- main.py
+++ main.py
@@ -2,101 +2,106 @@
 Author: MrDerpus
 Python version: 3.12.3
 
-TOME Parser v1.0.0
+TOME Parser v1.1.0
 Table Oriented Markup Encoding
 
 I just wanted a good looking data structure that is easy to use in python.
-Also because I keep seeing MEMEs 
-
-
-
-Reads a structured table with this format:
-	TableName[col1, col2, col3]:
-		value1, value2, value3
-		value1, value2, value3
-		...
-
-Syntax rules:
-	> Header defines the table name and ordered column list.
-	> Data rows map directly to the column list.
-	> Indentation is optional.
-	> Commas "," define the field.
-	> Leading and trailing whitespace is ignored.
-	> Lines beginning "#" or ";" are treated as comments.
-	> A line beginning with "--" will end the parsing early.
-	> Currently returns all values as strings.
-
-Proposed changes:
-	> Make values return their proper type.
-
-Returns a dictionary in this format:
-	{
-		"table_name": <name>,
-		0:{col1: value, col2: value, ...},
-		1{...},
-		...
-	}
-
-And can be accessed like this:
-	output = DataParser.read(./path/to/file/test.t)
-	table_name = output["TableName"]
-	first_item = output[0][...]
+Also because I keep seeing MEMEs about TOON.
 '''
-
-
-
-
+from sys import exit as kill
 class TOME:
 	
 	@staticmethod
 	def read(input_file:str):
 
 		with open(input_file, 'r') as file:
-			row_index = -1  # Tracks actual data rows (index starts at 0).
-			brackets = '[]' # Characters used to detect the header section.
+			syntax = {
+				'brackets':'[]',       # Characters used to detect the header section.
+				'comments':['#', ';'], # Comments, lines to ignore
+				'var-type':':',        # Separate values from their data types.
+				'end-parse': '!',      # Stop parsing file.
+				'new-table': '==',     # Start parsing new table.
+				'separator': ','       # The comma delimiter.
+			}
+
+			table:dict     = {}   # Table dictionary that is output.
+			row_index:int  = -1   # Tracks actual data rows (index starts at 0).
+			table_name:str = ''   # Name of current table.
+			values:list    = []   # Store values to be added to dictionary.
+			data_type:list = []   # Get datatype for table item.
+			columns:list   = []   # Store column names for tables.
+			table_type:str = ''   # Set to 'dynamic' by default
+
 			
-			# Read file line by line, removing the white space from the line.
+			# Read file line by line, removing the white space from the beginning & end of the line.
 			for line in file:
 				line = line.strip()
 
-				if line == '' or line is None: continue # skip empty line.
-				elif line[0] in ['#', ';']:    continue # skip commented lines.
-				elif line[:2].strip() == '--': return table # end file.
+				if line is None or line == '': row_index = -1; continue # Skip empty line.
+				elif line[0] in syntax['comments']: continue # Skip commented lines.
+				elif line[:2].strip() == syntax['new-table']: row_index = -1; continue # End current table parsing, and get ready to parse new table.
+				elif line[:2].strip() == syntax['end-parse']: return table   # End file parsing early, return current parsed data.
 				
 				row_index += 1
 
 				# Define table and column names
 				if row_index == 0:
-					table_name:str = line.split(brackets[0])[0].strip() # Collect table name.
-					columns:list   = line.split(brackets[0])[1].split(brackets[1])[0].split(',') # I dont suffer from Long-line-itis, I genuinely enjoy it.
-					table:dict = { 'table_name': table_name }
+					table_type = 'dynamic'
+					table_name = line.split(syntax['brackets'][0])[0].strip() # Collect table name.
+					columns    = line.split(syntax['brackets'][0])[1].split(syntax['brackets'][1])[0].split(syntax['separator']) # I dont suffer from Long-line-itis, I genuinely enjoy it.
+					data_type = []
 
-					# Cleanse items in columns.
-					columns = [ i.strip() for i in columns ]
-				
+					# create table name, and cleanse items for columns list.
+					table[table_name] = []
+					
+					
+					# if table is strongly typed, cast data types in a list to be accessed later in code.
+					for i in range(len(columns)):
+						if syntax['var-type'] in columns[i]:
+							table_type = 'strict'
+							_type = columns[i].split(syntax['var-type'])[1].strip().lower()
+							_valu = columns[i].split(syntax['var-type'])[0].strip()
+							
+							# if type is in accepted list, append type to data_type:list.
+							if _type in ['str', 'string', 'int', 'integer', 'float', 'boolean', 'bool']:
+								if   _type == 'string':  _type = 'str'
+								elif _type == 'boolean': _type = 'bool'
+								elif _type == 'integer': _type = 'int'
+								
+								data_type.append(_type)
+								columns[i] = _valu
+						
+						# Set type string if data type is not supported.
+						else:
+							data_type.append('str')
+							columns[i] = columns[i].strip()
 
+					#KEEP BELOW
+					#columns   = [ i.strip() for i in columns ]
 
-				# Collect values line and place them into dictionary
+				# Collect values line and place them into dictionary.
 				elif row_index > 0:
 					index = row_index-1
-					table[index] = {}
+					values = [ j.strip() for j in line.split(syntax['separator']) ]
 
-					# Cleanse values of rouge whitespace.
-					values = [ j.strip() for j in line.split(',') ]
+					# Cast variables as their defined data types, and append to dictionary.
+					if table_type == 'strict':
+						for i in range(len(values)):
+							
+							if data_type[i]   == 'int':   values[i] = int(values[i])
+							elif data_type[i] == 'float': values[i] = float(values[i])
+							elif data_type[i] == 'bool': 
+								if values[i].lower() in ['true', 'yes']:
+									values[i] = True
+								else:
+									values[i] = False
 
-					# Add values to dictionary.
-					table[index] = { columns[j]: values[j] for j in range(len(values)) }			
+							else: # Set data type to string to avoid errors.
+								values[i] = str(values[i])
+					
+					# Append to table dictionary.
+					table[table_name].append({ columns[j]: values[j] for j in range(len(values)) })
 			
+
 			# Return.
 			return table
-
-
-# Output debug mode
-
-if 1==2:
-
-	from rich.traceback import install; install(show_locals=True)
-	from rich.console   import Console; Print = Console().print
-
-	output = TOME.read('test.t')
-	Print(output)
