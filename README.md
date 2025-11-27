aca65476ff7e7488bc946e70be1d8ccb761ea1d5
MrDerpus <47982738+MrDerpus@users.noreply.github.com>
2025-11-28 02:06:33 +1100

    v1.2.0
    Added TOME.write() function to create files.
    Added TOME <-> JSON compatibility.
    Fixed strict typing, now providing proper error messages when an error occurs.
    Fixed several bugs.

--- README.md
+++ README.md
@@ -4,32 +4,32 @@
 Author: MrDerpus
 Python version: 3.12.3
 
-TOME Parser v1.1.0
+TOME Parser v1.2.0
 Table Oriented Markup Encoding.
 
-I just wanted a good looking data structure that is easy read.
+I just wanted a good looking data structure that is easy to read.
 Also because I saw MEMEs about Ai bros mentioning: TOON.
 So I was inspired to create my own parser for my custom data structure.
 
-This combines the simplicity of:
-- writing in a SQL-like database structure,
-- CSV rows for data entry,
-- and receiving clean python friendly dictionaries.
+TOME combines:
+- SQL-like table headers.
+- CSV row entry.
+- Clean Python dictionaries as output. 
 ```
 
-# 🪄 A super big update! v1.1.0
+# 🪄 Another super big update! v1.2.0
 What was added? <br>
-* Multi-table support.
-* Strict typing for table headers (Optional).
-* Refactored syntax dictionary.
-* Auto-detect new tables (Only if formatting is compatible).
-* Clean Python dictionary output structure.
+* TOME.write, allows you to write/append to TOME files.
+* TOME <=> JSON compatibility !!!
+* Strict typing now works 100% You WILL comply! (optional)
+* Added detailed error messages.
+* Fixed bugs that caused crashes when parsing input files.
 
 ### Make sure to read the rules to see how to use these new changes.
 
 
 
-## 📜 Rules:
+# 📜 Rules:
 * Header defines the table name and ordered column list.
 * Data rows map directly to the column list.
 * Indentation is optional.
@@ -37,39 +37,18 @@ What was added? <br>
 * Leading and trailing whitespace is purged for your convenience.
 * Lines beginning "#" or ";" are treated as comments.
 * A line beginning with "!"  will end the parsing early.
-* A line beginning with "==" will tell the parser to start paring a new data table.
+* A line beginning with "==" will tell the parser to start parsing a new data table.
 * A blank line separating two tables will also automatically trigger parsing for another data table.
 * By default, all values are returned as strings, unless specified otherwise.
-
-
-## TOME returns a dictionary in this format:
-```python
-{
-    'items': [
-        {
-            'serial': '2ffc-4eb3-8a2f',
-            'item': 'keyboard',
-            'qty': '50',
-            'price': '36.99',
-            'sale': 'true'
-        },
-        {
-            'serial': 'a6eb-4c71-b501',
-			...
-		}
-	]
-}
-```
-
-## Accessing values in Python:
-```python
-output = TOME.read('./path/to/file/test.t')
-first_item_serial = output['items'][0]['serial']
-```
 ---
 
-## Demos:
+# Demos:
+
+## TOME File structure:
 ```ini
+# This is a commented line.
+; this is also a commented line.
+
 # Default parsing, will return all values as strings:
 employees[uuid, name, age]
 	c0fb3d3b-2ffc-4eb3-8a2f-8177aeebecd3, John Doe, 35
@@ -78,14 +57,92 @@ employees[uuid, name, age]
 
 # Strict parsing, only selected columns are type-cast.
 # Columns without a type remain as strings.
-items[serial:str, item, qty:int, price:float, sale:bool]:
+inventory[serial:str, item, qty:int, price:float, sale:bool]:
 	2ffc-4eb3-8a2f, keyboard,  50,  36.99,   true
 	a6eb-4c71-b501, monitor,   27,  139.98,  true
 	a512-4eda-b55f, mouse,     23,  20.99,   false
+
+! ;<-- End parsing here and return everything above this line.  
+
+; An example of using the wrong data type in a strict table.
+items[name:str, price:int]:
+    Hammer, 19
+    Saw, 12
+    Wrench, cheap
+    ;        ^^^ this will throw a TOMEparseError
+    ; as it is not the correct data type
+
+; Will provide this error message in your console:
+; TOMEparseError @ line 12 in test.tome:
+; price expected data type: int, but 'cheap' was given instead.
 
 ; Supported data types:
 ; String/str
 ; Integer/int
 ; Boolean/bool
 ; Float
+```
+
+## Python
+
+```python
+from main import TOME
+import json
+
+# Accessing values in Python: ------------
+data:dict = TOME.read('./path/to/file/test.tome')
+''' returns dictionary in this format:
+{
+    'items': [
+        {
+            'serial': '2ffc-4eb3-8a2f',
+            'item': 'keyboard',
+            'qty': 50,
+            'price': 36.99,
+            'sale': True
+        },
+        {
+            'serial': 'a6eb-4c71-b501',
+			...
+		}
+	]
+}
+'''
+
+first_item_serial = data['items'][0]['serial']
+print(first_item_serial)
+# output: '2ffc-4eb3-8a2f' ---------------
+
+# Editing/Adding existing values and creating a file with TOME.write()
+data['new_table'] = [
+    {
+        'name': 'John Doe',
+        'age': 28,
+        'favourite_song': 'Simple Man - Shinedown'
+    },
+    {
+        'name': 'Jane Doe',
+        'age': 30,
+        'favourite_song': 'Higher - Creed'
+    },
+    {
+        'name': 'Will Doe',
+        'age': 26,
+        'favourite_song': 'H. - Tool'
+    }
+]
+
+TOME.write('new_file.tome', data, 'w')
+
+
+# Converting formats . . .
+# TOME to JSON
+data = TOME.read('test.tome')
+with open('tome-to-json.json', 'w') as f:
+	json.dump(data, f, indent=4)
+
+# JSON to TOME
+with open('tome-to-json.json', 'r') as f:
+	data = json.load(f)
+TOME.write('json-to-tome.tome', data, 'w')
 ```
--- main.py
+++ main.py
@@ -2,17 +2,53 @@
 Author: MrDerpus
 Python version: 3.12.3
 
-TOME Parser v1.1.0
+TOME Parser v1.2.0
 Table Oriented Markup Encoding
 
 I just wanted a good looking data structure that is easy to use in python.
 Also because I keep seeing MEMEs about TOON.
 '''
-from sys import exit as kill
 class TOME:
-	
 	@staticmethod
-	def read(input_file:str):
+	def read(input_file:str) -> dict:
+
+
+		# Present error message to terminal. (used inside strict_cast) ----
+		def strict_type_error(expected:str, value:str, column:str, file:str, line:int) -> None:
+			raise ValueError(
+				f'\n\nTOMEparseError @ line {line} in {file}:\n'
+				f'{column} expected data type: {expected}, but \'{value}\' was given instead.\n'
+			)
+		
+		# Cast data types for columns defined in table header.
+		def strict_cast(value:str, type_name:str, column:str, file:str, line:int): # returns str, int, float or bool
+
+			type_map:dict = { # Data type mapping.
+				'str': str,
+				'int': int,
+				'float': float,
+				'bool': bool
+			}
+
+			# class empty values as empty
+			if value == '' and type_name != 'str':
+				strict_type_error(type_name, '(empty)', column, file, line_number)
+
+			# Return bool
+			if type_name == 'bool':
+				v = value.lower()
+				if v in ['true', 'yes', '1']: return True
+				else: return False
+				strict_type_error('bool', value, column, file, line_number)
+
+			# int, float or str
+			try:
+				return type_map[type_name](value)
+			except:
+				strict_type_error(type_name, value, column, file, line_number)
+		# -----------------------------------------------------------------
+
+
 
 		with open(input_file, 'r') as file:
 			syntax = {
@@ -26,11 +62,15 @@ class TOME:
 
 			table:dict     = {}   # Table dictionary that is output.
 			row_index:int  = -1   # Tracks actual data rows (index starts at 0).
+			line_number:int = 1   # Keeps track of current line number.
 			table_name:str = ''   # Name of current table.
 			values:list    = []   # Store values to be added to dictionary.
 			data_type:list = []   # Get datatype for table item.
 			columns:list   = []   # Store column names for tables.
-			table_type:str = ''   # Set to 'dynamic' by default
+			table_type:str = ''   # Set to 'dynamic' by default.
+			_type:str      = ''   # Holds current set data type for row item in header. (Only used in table_type = strict)
+			_value:str     = ''   # Holds variable name. (Only used in table_type = strict)
+
 
 			
 			# Read file line by line, removing the white space from the beginning & end of the line.
@@ -47,9 +87,17 @@ class TOME:
 				# Define table and column names
 				if row_index == 0:
 					table_type = 'dynamic'
+
+
+					if syntax['brackets'][0] not in line or syntax['brackets'][1] not in line:
+						raise ValueError(
+							f'\n\nTOMEparseError @ line {line_number} in {input_file}:\n'
+							f'Malformed table header.\n'
+						)
+
 					table_name = line.split(syntax['brackets'][0])[0].strip() # Collect table name.
 					columns    = line.split(syntax['brackets'][0])[1].split(syntax['brackets'][1])[0].split(syntax['separator']) # I dont suffer from Long-line-itis, I genuinely enjoy it.
-					data_type = []
+					data_type  = []
 
 					# create table name, and cleanse items for columns list.
 					table[table_name] = []
@@ -59,8 +107,8 @@ class TOME:
 					for i in range(len(columns)):
 						if syntax['var-type'] in columns[i]:
 							table_type = 'strict'
-							_type = columns[i].split(syntax['var-type'])[1].strip().lower()
-							_valu = columns[i].split(syntax['var-type'])[0].strip()
+							_type  = columns[i].split(syntax['var-type'])[1].strip().lower()
+							_value = columns[i].split(syntax['var-type'])[0].strip()
 							
 							# if type is in accepted list, append type to data_type:list.
 							if _type in ['str', 'string', 'int', 'integer', 'float', 'boolean', 'bool']:
@@ -69,39 +117,107 @@ class TOME:
 								elif _type == 'integer': _type = 'int'
 								
 								data_type.append(_type)
-								columns[i] = _valu
+								columns[i] = _value
+							
+							else:
+								raise ValueError(
+									f'\n\nTOMEparseError @ line {line_number} in {input_file}, in Table [\'{table_name}\']:\n'
+									f'Invalid data type \'{_type}\', expected only: \n'
+									'[String/str, Integer/int, float, Boolean/bool].\n'
+								)
 						
 						# Set type string if data type is not supported.
 						else:
 							data_type.append('str')
 							columns[i] = columns[i].strip()
 
-					#KEEP BELOW
-					#columns   = [ i.strip() for i in columns ]
 
 				# Collect values line and place them into dictionary.
 				elif row_index > 0:
 					index = row_index-1
 					values = [ j.strip() for j in line.split(syntax['separator']) ]
 
+					if len(values) != len(columns):
+						raise ValueError(
+							f'\n\nTOMEparseError @ line {line_number} in {input_file}:\n'
+							f'Table: [\'{table_name}\'] has {len(columns)} columns defined in header but has {len(values)} values.\n'
+						)
+
 					# Cast variables as their defined data types, and append to dictionary.
 					if table_type == 'strict':
 						for i in range(len(values)):
-							
-							if data_type[i]   == 'int':   values[i] = int(values[i])
-							elif data_type[i] == 'float': values[i] = float(values[i])
-							elif data_type[i] == 'bool': 
-								if values[i].lower() in ['true', 'yes']:
-									values[i] = True
-								else:
-									values[i] = False
 
-							else: # Set data type to string to avoid errors.
-								values[i] = str(values[i])
-					
+							values[i] = strict_cast(
+								values[i],
+								data_type[i],
+								columns[i],
+								input_file,
+								line_number
+							)
+
 					# Append to table dictionary.
 					table[table_name].append({ columns[j]: values[j] for j in range(len(values)) })
+				
+				line_number += 1
 			
-
 			# Return.
 			return table
+
+
+
+	@staticmethod
+	def write(output_file:str, table:dict, mode:str='a') -> None:
+
+		type_map:dict = { # Data type mapping.
+			str: 'str',
+			int: 'int',
+			float: 'float',
+			bool: 'bool'
+		}
+		typed_columns:list = [] # Holds column names with their given data type.
+		columns:list       = [] # Holds Columns names grabbed from first row of dictionary.
+		values:list        = [] # Holds the row data.
+		lines:list         = [] # Holds output file data before writing to file.
+		types:list         = [] # Holds given data types for each column based on the row of a table. 
+
+
+
+		# Return TOME type string based on Python type.
+		def infer_type(value):
+			return type_map.get(type(value), 'str')
+			# str is the default data type returned using TOME.write(), unless assigned in .tome file
+
+		# Convert Python values into TOME compatible strings / data types.
+		def format_value(value) -> str:
+			if isinstance(value, bool):
+				return str(value).upper()
+			return str(value)
+
+		for table_name, rows in table.items():
+
+			# Skip blank lines.
+			if not rows: continue
+
+			# Extract column order from the first row.
+			columns = list(rows[0].keys())
+
+			# Assign data types from the first row.
+			types = [infer_type(rows[0][column]) for column in columns]
+
+			# Build typed header: column:type
+			typed_columns = [ f'{column}:{types[i]}' for i, column in enumerate(columns) ]
+
+			# Append header line to output.
+			lines.append(f'\n\n{table_name}[{", ".join(typed_columns)}]:')
+
+			# Append rows to output.
+			for row in rows:
+				values = [format_value(row[column]) for column in columns]
+				lines.append('\t' + ', '.join(values))
+
+			# Add blank line between tables to avoid dict / table reading conflicts.
+			lines.append('')
+
+		# Write to output file.
+		with open(output_file, mode) as f:
+			f.write('\n'.join(lines))
